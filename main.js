f2d09e2464bbf05bc2a8d13218c077bbdb1c664a
Benjamin Saenz <bsaenz454@gmail.com>
2024-07-11 23:48:50 -0400

    Added boardgame assets, formatted landing page

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas') });
-renderer.setSize(window.innerWidth/1.5, window.innerHeight/1.5);
-renderer.setClearColor(new THREE.Color(0xf7cb46)); // hex for white
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+renderer.setClearColor(new THREE.Color(0xfcf4e7)); // hex for white
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // enable damping
@@ -45,7 +45,7 @@ loader.load(
 );
 
 // Set camera position
-camera.position.set(0.45,1.125,1.125)
+camera.position.set(0.293,0.731,0.731)
 
 function animate() {
   requestAnimationFrame(animate);
